919bbfe6950ad2083f75710757b2e229fddbaf25
Alex Lee <dung.le@snapmart.ph>
2022-07-12 14:21:18 +0700

    test tailwinds components - ok

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,20 @@
 import * as React from 'react'
 import Layout from '../components/layout'
+import Test from '../components/test'
 
 const IndexPage = ({ data, location }) => {
-  return (
-    <Layout title={'Overreacted'} location={location}>
-      <main className='m-5'>
-        <title>Home Page</title>
-        <h1 className="text-xl font-normal no-underline text-red-400">Hello world!</h1>
-      </main>
-    </Layout>
-  )
+  return <Test />
+  // return (
+  //   <Layout title={'Overreacted'} location={location}>
+  //     <main className="m-5">
+  //       <title>Home Page</title>
+  //       <h1 className="text-xl font-normal no-underline text-red-400">
+  //         Hello world!
+  //       </h1>
+  //     </main>
+      
+  //   </Layout>
+  // )
 }
 
 export default IndexPage
