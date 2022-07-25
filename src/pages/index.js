a5dbf64494b690ee4a6372062c424c2d316c35a3
Alex Lee <dung.le@snapmart.ph>
2022-07-25 10:15:04 +0700

    clear alls

--- src/pages/index.js
+++
@@ -1,7 +0,0 @@
-import * as React from 'react'
-
-const IndexPage = () => {
-  return <div className="m-5">Homepage</div>
-}
-
-export default IndexPage
--- src/pages/templates/blog-post.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-const Template = ({ data }) => {
-  return <div>Template file</div>
-}
-
-export default Template
