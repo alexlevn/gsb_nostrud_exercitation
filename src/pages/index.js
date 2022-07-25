9b9418ee9244b5b9637c048026b987e165ec5099
Alex Lee <dung.le@snapmart.ph>
2022-07-25 16:36:10 +0700

    save

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,48 @@
+import { graphql } from 'gatsby'
 import * as React from 'react'
 
-const IndexPage = () => {
-  return <main>Home Page !</main>
+const IndexPage = ({ data, location }) => {
+  const siteTitle = data.site.siteMetadata?.title || `Title`
+  const posts = data.allMarkdownRemark.nodes
+
+  return (
+    <main>
+      Home Page !
+      <br />
+      {posts.map((post) => {
+        const title = post.frontmatter.title || post.fields.slug
+        return (
+          <div key={post.fields.slug}>
+            <h2>{title}</h2>
+            <p>{post.frontmatter.date}</p>
+            <p>{post.excerpt}</p>
+          </div>
+        )
+      })}
+    </main>
+  )
 }
 
 export default IndexPage
+
+export const pageQuery = graphql`
+  query {
+    site {
+      siteMetadata {
+        title
+      }
+    }
+    allMarkdownRemark(sort: { fields: [frontmatter___date], order: DESC }) {
+      nodes {
+        excerpt
+        fields {
+          slug
+        }
+        frontmatter {
+          date(formatString: "MMMM DD, YYYY")
+          title
+          description
+      }
+    }
+  }
+`
