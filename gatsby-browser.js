911658eababaa3d48cc38e99ef13b4768ce41806
Alex Lee <dung.le@snapmart.ph>
2022-07-26 10:47:36 +0700

    add header & custom css

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,2 +1,15 @@
 import './global.css'
 import 'prismjs/themes/prism.css'
+
+// Custom css
+
+// custom typefaces
+import 'typeface-montserrat'
+import 'typeface-merriweather'
+// normalize CSS across browsers
+import './src/normalize.css'
+// custom CSS styles
+import './src/style.css'
+
+// Highlighting for code blocks
+import 'prismjs/themes/prism.css'
--- src/components/layout.js
+++ src/components/layout.js
@@ -1,8 +1,19 @@
+import { Link } from 'gatsby'
 import * as React from 'react'
 
 const Layout = ({ location, title, children }) => {
   const rootPath = `${__PATH_PREFIX__}/`
   const isRootPath = location.pathname === rootPath
+  let header
+
+  if (isRootPath) {
+    header = (
+      <h1 className="main-heading">
+        <Link to="/">{title}</Link>
+      </h1>
+    )
+  } 
+
   return (
     <div className="mt-5 mx-5 border-2 p-3 border-gray-400-500 ">
       <div className="text-red-500">Layout:</div>
