4d7f52f02dd39338d4a81cd055fbd36287ad1db2
Alex Lee <dung.le@snapmart.ph>
2022-07-27 15:25:20 +0700

    layout

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -1,20 +1,75 @@
-import { graphql } from 'gatsby'
+import { graphql, Link } from 'gatsby'
 import * as React from 'react'
 import Layout from '../components/layout'
+import Bio from '../components/bio'
 
 const BlogPostTemplate = ({ data, location }) => {
   const post = data.markdownRemark
   const siteTitle = data.site.siteMetadata.title
+  const { previous, next } = data
 
   return (
     <Layout location={location} title={siteTitle}>
-      <article className='text-pc-light'>
+      <article className="text-pc-light">
+        
+          <div className="grid gap-4">
+            <div className="bg-pc-darker rounded-md col-span-1 col-auto">
+              {previous && (
+                <Link to={previous.fields.slug} rel="prev">
+                  ← {previous.frontmatter.title}
+                </Link>
+              )}
+            </div>
+            <div className="bg-pc-darker rounded-md col-span-1">
+              {next && (
+                <Link to={next.fields.slug} rel="next">
+                  {next.frontmatter.title} →
+                </Link>
+              )}
+            </div>
+          </div>
+
+          <div className='mt-10 block'>
+            Hello
+          </div>
+        
+
         <header>
-          <h1 className='text-3xl font-medium text-pc-yellow'>{post.frontmatter.title}</h1>
+          <h1 className="text-3xl font-medium text-pc-yellow">
+            {post.frontmatter.title}
+          </h1>
           <p>{post.frontmatter.date}</p>
         </header>
 
-        <section dangerouslySetInnerHTML={{ __html: post.html }} />
+        <section
+          dangerouslySetInnerHTML={{ __html: post.html }}
+          itemProp="acticleBody"
+        />
+
+        <hr />
+
+        <footer>
+          <Bio />
+        </footer>
+
+        <nav className="blog-post-nav">
+          <ul>
+            <li>
+              {previous && (
+                <Link to={previous.fields.slug} rel="prev">
+                  ← {previous.frontmatter.title}
+                </Link>
+              )}
+            </li>
+            <li>
+              {next && (
+                <Link to={next.fields.slug} rel="next">
+                  {next.frontmatter.title} →
+                </Link>
+              )}
+            </li>
+          </ul>
+        </nav>
       </article>
     </Layout>
   )
